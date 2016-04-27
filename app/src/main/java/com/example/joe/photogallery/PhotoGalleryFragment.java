4fea161382270a3e035c325b7872cec32a12c5d6
Joe Stevens <joseph.stevens34@gmail.com>
2016-04-27 13:07:28 -0400

    Adding adapter update code

--- app/src/main/java/com/example/joe/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/example/joe/photogallery/PhotoGalleryFragment.java
@@ -99,12 +99,17 @@ public class PhotoGalleryFragment extends Fragment{
         }
     }
 
-    private class FetchItemsTask extends AsyncTask<Void, Void,Void>{
+    private class FetchItemsTask extends AsyncTask<Void, Void,List<GalleryItem>>{
 
         @Override
-        protected Void doInBackground(Void... params) {
-            new FlickrFetchr().fetchItems();
-            return null;
+        protected List<GalleryItem> doInBackground(Void... params) {
+            return new FlickrFetchr().fetchItems();
+        }
+
+        @Override
+        protected void onPostExecute(List<GalleryItem> items) {
+            mItems = items;
+            setupAdapter();
         }
     }
 }
