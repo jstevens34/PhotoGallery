b3873d081574975d6e5e5b0a88a7454912513389
Joe Stevens <joseph.stevens34@gmail.com>
2016-08-06 02:18:31 -0400

    Added sharedPreference for search

---
+++ app/src/main/java/com/example/joe/photogallery/QueryPreferences.java
@@ -0,0 +1,23 @@
+package com.example.joe.photogallery;
+
+import android.content.Context;
+import android.preference.PreferenceManager;
+
+/**
+ * Created by Joe on 8/6/2016.
+ */
+public class QueryPreferences {
+    private static final String PREF_SEARCH_QUERY = "searchQuery";
+
+    public static String getStoredQuery(Context context){
+        return PreferenceManager.getDefaultSharedPreferences(context)
+                .getString(PREF_SEARCH_QUERY, null);
+    }
+
+    public static void setStoredQuery(Context context, String query){
+        PreferenceManager.getDefaultSharedPreferences(context)
+                .edit()
+                .putString(PREF_SEARCH_QUERY, query)
+                .apply();
+    }
+}
