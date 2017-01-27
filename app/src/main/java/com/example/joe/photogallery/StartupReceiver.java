e18d3f9d8bd782b9c445e0b60b4fd7e686f303ea
Joe Stevens <joseph.stevens34@gmail.com>
2017-01-26 23:53:43 -0500

    Create broadcast receiver

---
+++ app/src/main/java/com/example/joe/photogallery/StartupReceiver.java
@@ -0,0 +1,20 @@
+package com.example.joe.photogallery;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+/**
+ * Created by Joe on 1/26/2017.
+ */
+
+public class StartupReceiver extends BroadcastReceiver {
+
+    private static final String TAG = "StartupReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.i(TAG, "Received broadcast intent: " + intent.getAction());
+    }
+}
