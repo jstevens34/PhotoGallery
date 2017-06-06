51ff9f571a88f29387006befd29a1b37aa75f296
Joe Stevens <joseph.stevens34@gmail.com>
2017-06-05 21:56:16 -0400

    28.1 Add owner and build uri

--- app/src/main/java/com/example/joe/photogallery/GalleryItem.java
+++ app/src/main/java/com/example/joe/photogallery/GalleryItem.java
@@ -1,5 +1,7 @@
 package com.example.joe.photogallery;
 
+import android.net.Uri;
+
 import com.google.gson.annotations.SerializedName;
 
 /**
@@ -12,6 +14,9 @@ public class GalleryItem {
     private String mId;
     @SerializedName("url_s")
     private String mUrl;
+
+    @SerializedName("owner")
+    private String mOwner;
 
     @Override
     public String toString() {
@@ -40,5 +45,21 @@ public class GalleryItem {
 
     public void setUrl(String url) {
         mUrl = url;
+    }
+
+    public String getOwner() {
+        return mOwner;
+    }
+
+    public void setOwner(String owner) {
+        mOwner = owner;
+    }
+
+    public Uri getPhotoPageUri() {
+        return Uri.parse("http://www.flickr.com/photos/")
+                .buildUpon()
+                .appendPath(mOwner)
+                .appendPath(mId)
+                .build();
     }
 }
